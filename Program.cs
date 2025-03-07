89a701ad5170cf54c406a54287390493fe3d0416
froby2024 <167176248+froby2024@users.noreply.github.com>
2025-03-07 11:23:07 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
 while (max == -1 || counter < max)
 {
- Console.WriteLine($"Counter: {++counter}");
+ Console.WriteLine($"Contatore: {++counter}");
  await Task.Delay(TimeSpan.FromMilliseconds(1_000));
 }
 
